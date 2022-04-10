7d9a5d96702d2b8d4d285e85452f3a37eafb79ed
Suat Özkaya <suatozkaya@gmail.com>
2022-04-10 17:34:33 +0300

    Lift state up

--- app/src/main/java/com/ozkayas/jetpactipapp/MainActivity.kt
+++ app/src/main/java/com/ozkayas/jetpactipapp/MainActivity.kt
@@ -1,23 +1,32 @@
 package com.ozkayas.jetpactipapp
 
+import android.net.LocalSocket
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.text.KeyboardActions
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.MutableState
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
+import androidx.compose.ui.ExperimentalComposeUiApi
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalSoftwareKeyboardController
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import com.ozkayas.jetpactipapp.components.InputField
 import com.ozkayas.jetpactipapp.ui.theme.JetpactipappTheme
 
 class MainActivity : ComponentActivity() {
@@ -63,9 +72,31 @@ fun TopHeader(totalPerPerson: Double = 1.0) {
     }
 }
 
+@OptIn(ExperimentalComposeUiApi::class)
 @Preview
 @Composable
 fun MainContent() {
+    BillForm(){
+        billAmount ->
+        Log.d("TAG", "MainContent: $billAmount")
+    }
+
+}
+
+@OptIn(ExperimentalComposeUiApi::class)
+@Composable
+fun BillForm(modifier: Modifier = Modifier,
+            onValChange: (String) -> Unit = {}){
+
+    var totalBillState = remember {
+        mutableStateOf("")
+    }
+    var validState = remember(totalBillState.value) {
+        totalBillState.value.trim().isNotEmpty()
+    }
+    val keyboardController = LocalSoftwareKeyboardController.current
+
+
     Surface(modifier = Modifier
         .padding(2.dp)
         .fillMaxWidth(),
@@ -73,10 +104,15 @@ fun MainContent() {
         border = BorderStroke(width = 1.5.dp, color = Color.LightGray)
     ) {
         Column() {
-            Text(text = "Hello")
-            Text(text = "Hello")
-            Text(text = "Hello")
-            Text(text = "Hello")
+            InputField(valueState = totalBillState, labelId ="Enter Bill", enabled = true, isSingleLine = true,
+                onAction = KeyboardActions{
+                    if (!validState) return@KeyboardActions
+                    onValChange(totalBillState.value.trim())
+                    keyboardController?.hide()
+                }
+            )
+
         }
     }
+
 }
